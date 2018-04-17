c8981ecc9a1b753272d57294ffab68acbfe303b7
Dmitriy <dima_tarelkin@mail.ru>
2018-04-18 00:30:47 +0300

    start NSString

---
+++ NSString/NSString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSString
+//
+//  Created by Dmitriy Tarelkin on 18/4/18.
+//  Copyright © 2018 Dmitriy Tarelkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSString/NSString/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  NSString
+//
+//  Created by Dmitriy Tarelkin on 18/4/18.
+//  Copyright © 2018 Dmitriy Tarelkin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
